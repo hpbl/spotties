1f8d52e0807b9a9960af8caba75b6ee7e1afdc68
Victor Miranda de Melo <vmm@WINDOWS.CIN.UFPE.BR>
2018-10-04 14:16:25 -0300

    spotify app data

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import './App.css';
 import logo from './logo.svg';
 import setPageTitle from './helper';
 
+// const clientId = '139b330668894b13b5d959cb947966da';
+// const clientSecret = '0046d6777a414dbf90434dc6fdd0b0c0';
+// const redirectURI = 'https://hpbl.github.io/spotties/';
+// const scope = 'user-read-private user-read-email user-read-playback-state';
+
 function App() {
   setPageTitle();
   return (
