2206834c8912d292022d7e3c3dc67b71fff4da20
vmmelo <vmmelo1@gmail.com>
2020-09-12 16:42:53 +0000

    Deploying to gh-pages from  @ cff26eef31848dcdc54dc21a4062f126d27a2176 🚀

---
+++ precache-manifest.780b396685bd5338d2da3590b755c38d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b502a8ef30d0cd1985fbde5e15ebf9",
+    "url": "/spotties/index.html"
+  },
+  {
+    "revision": "1e7878fed64c2d715f9a",
+    "url": "/spotties/static/css/main.bb494bc5.chunk.css"
+  },
+  {
+    "revision": "704da33fe8d15ca017c3",
+    "url": "/spotties/static/js/2.464bd16d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spotties/static/js/2.464bd16d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e7878fed64c2d715f9a",
+    "url": "/spotties/static/js/main.f7c144df.chunk.js"
+  },
+  {
+    "revision": "c97d4bd640134e04622d",
+    "url": "/spotties/static/js/runtime-main.3f20858f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spotties/static/media/logo.5d5d9eef.svg"
+  }
+]);
